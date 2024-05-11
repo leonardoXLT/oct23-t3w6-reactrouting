71f3f0c7a435745f2a479953fca13a917a59d09b
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-11 11:01:59 +1000

    feat: child routes working

--- src/App_ComponentRouting.jsx
+++ src/App_ComponentRouting.jsx
@@ -1,22 +1,27 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
+import BaseLayout from './pages/_baseLayout'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/about/services' element={<AboutPage />} />
-          <Route path='/contact' element={<ContactPage />} />
-          {/* Need a page for any not-matched route */}
-          <Route path='*' element={<PageNotFound />} /> 
+
+          <Route path='/' element={<BaseLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path='/about/services' element={<AboutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='*' element={<PageNotFound />} /> 
+          </Route>
+          
 
           {/* <Route path='/' element={<HomePage />}>
             <Route path='about'>
@@ -25,8 +30,10 @@ function App() {
             <Route path='contact' element={<ContactPage />} />
           </Route> */}
         </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
+
---
+++ src/App_ObjectRouting.jsx
@@ -0,0 +1,41 @@
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './App.css'
+import AboutPage from './pages/About'
+import ContactPage from './pages/Contact'
+import HomePage from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
+import BaseLayout from './pages/_baseLayout'
+
+
+const someRouterBlahBlah = createBrowserRouter([
+  {
+    path:"/",
+    element: <BaseLayout />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "about",
+        element: <AboutPage />
+      }, 
+      {
+        path: "contact",
+        element: <ContactPage />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={someRouterBlahBlah} />
+    </>
+  )
+}
+
+export default App
