a8d26e72823a05062a225b421d298742a3c1137e
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-11 10:40:47 +1000

    feat: separate routes from router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AboutPage from './pages/About'
 import ContactPage from './pages/Contact'
 import HomePage from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
   
 
   return (
     <>
-      <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
+          <Route path='/about/services' element={<AboutPage />} />
           <Route path='/contact' element={<ContactPage />} />
+          {/* Need a page for any not-matched route */}
+          <Route path='*' element={<PageNotFound />} /> 
+
+          {/* <Route path='/' element={<HomePage />}>
+            <Route path='about'>
+              <Route path='services' element={<AboutPage />} />
+            </Route>
+            <Route path='contact' element={<ContactPage />} />
+          </Route> */}
         </Routes>
-      </BrowserRouter>
     </>
   )
 }
